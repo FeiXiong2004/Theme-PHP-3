74a7568bf5e6901f9b95dbc9ec8ff093672784be
FeiXiong2004 <hunghn2004@gmail.com>
2024-08-07 22:23:27 +0700

    Folder  asset ném vào public,Folder admin ném vào views

---
+++ admin/products/edit.blade.php
@@ -0,0 +1,69 @@
+@extends('admin.layouts.layout')
+@section('title', 'Product Update')
+@section('body')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Product Update</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Text Editors</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-outline card-info">
+                        <div class="card-header">
+                            <h3 class="card-title">
+                                Product Update
+                            </h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                           
+                            
+                            
+
+                        </div>
+
+                    </div>
+                </div>
+                <!-- /.col-->
+            </div>
+
+        </section>
+        <!-- /.content -->
+    </div>
+@endsection
+
+@section('script')
+    <!-- Summernote -->
+    <script src="{{ asset('/asset/admin/') }}/plugins/summernote/summernote-bs4.min.js"></script>
+    <!-- Page specific script -->
+    <script>
+        $(function() {
+            // Summernote
+            $('#summernote').summernote()
+            $('#material').summernote()
+            // CodeMirror
+            // CodeMirror.fromTextArea(document.getElementById("codeMirrorDemo"), {
+            //     mode: "htmlmixed",
+            //     theme: "monokai"
+            // });
+        });
+    </script>
+
+   
+@endsection
